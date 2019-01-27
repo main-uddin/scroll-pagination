5fc357e0d2e84c6f8158b87f090db824e20be146
Main Uddin <mdmain74@gmail.com>
2019-01-27 23:23:28 +0600

    add scroll pagination

--- src/Home.js
+++ src/Home.js
@@ -5,14 +5,15 @@ class Home extends Component {
   state = {
     photos: [],
     queryStr: 'sexy',
-    page: 1
+    page: 1,
+    notSent: true
   };
 
   componentDidMount() {
     fetch(
       `https://cors-anywhere.herokuapp.com/https://api.pexels.com/v1/search?query=${
         this.state.queryStr
-      }+query&per_page=100&page=${this.state.page}`,
+      }+query&per_page=20&page=${this.state.page}`,
       {
         headers: {
           Accept: 'application/json',
@@ -29,43 +30,52 @@ class Home extends Component {
         )
       );
 
-    window.addEventListener('scroll', this.handleScroll)
+    window.addEventListener('scroll', this.handleScroll);
   }
 
   handleScroll = e => {
-      let scrlHeight = document.scrollingElement.scrollHeight
-      let clntHeight = document.scrollingElement.clientHeight
-      let goingToScroll = scrlHeight - clntHeight
-      let scrlTop = document.scrollingElement.scrollTop
-      let parcentage = Math.ceil(scrlTop / goingToScroll * 100)
-      // console.log(`${parcentage}%`)
-      if(parcentage === 94) {
-        console.log('94')
-        fetch(
-          `https://cors-anywhere.herokuapp.com/https://api.pexels.com/v1/search?query=hill+query&per_page=100&page=${this.state.page}`,
-          {
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-              Authorization:
-                '563492ad6f9170000100000154dfd7cac5734ffbab147db82c7618e9'
-            }
+    let scrlHeight = document.scrollingElement.scrollHeight;
+    let clntHeight = document.scrollingElement.clientHeight;
+    let goingToScroll = scrlHeight - clntHeight;
+    let scrlTop = document.scrollingElement.scrollTop;
+    let parcentage = Math.ceil((scrlTop / goingToScroll) * 100);
+    // console.log(`${parcentage}%`)
+    if (parcentage === 96 && this.state.notSent) {
+      console.log(this.state.page);
+      this.setState({
+        page: this.state.page + 1,
+        notSent: false
+      });
+      fetch(
+        `https://cors-anywhere.herokuapp.com/https://api.pexels.com/v1/search?query=${
+          this.state.photos
+        }+query&per_page=20&page=${this.state.page}`,
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization:
+              '563492ad6f9170000100000154dfd7cac5734ffbab147db82c7618e9'
           }
-        )
-          .then(res => res.json())
-          .then(data =>
-            data.photos.forEach(url =>
-              this.setState({ photos: this.state.photos.concat(url.src.portrait) })
-            )
-          );
-      }
-  }
+        }
+      )
+        .then(res => res.json())
+        .then(data =>
+          data.photos.forEach(url =>
+            this.setState({
+              photos: this.state.photos.concat(url.src.portrait),
+              notSent: true
+            })
+          )
+        );
+    }
+  };
 
   render() {
     return (
       <div className="photos">
-        {this.state.photos.map(e => (
-          <img className="img" src={e} alt="not found" />
+        {this.state.photos.map(images => (
+          <img className="img" src={images} alt="not found" />
         ))}
       </div>
     );
